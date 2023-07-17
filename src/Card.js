e4f6a320746ddf9e9ff73edd9db5c3e6f3fefb38
Ivan Parkhomenko <96525894+light-pro-grammer@users.noreply.github.com>
2023-07-17 10:40:26 +0300

    add code highlighting
    used highlight.js library + tailwind typography plugin to style markdown input from cards

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import { AiOutlineMinus } from 'react-icons/ai';
 import { FaRegTrashAlt } from 'react-icons/fa';
 import { FaEdit } from 'react-icons/fa';
 import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import './styles.css';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark.css';
 
 const Card = ({ card, updateCard, deleteCard, changeRating }) => {
   const [editMode, setEditMode] = useState(false);
@@ -22,30 +22,46 @@ const Card = ({ card, updateCard, deleteCard, changeRating }) => {
     setEditMode(false);
   };
 
+  useEffect(() => {
+    document.querySelectorAll('pre code').forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+  }, []);
+
   return (
     <li className="border border-black p-4 my-2 w-full rounded flex justify-between bg-slate-200">
       {editMode ? (
-        <div className="flex flex-col">
-          <textarea
-            className="p-1"
-            value={input.question}
-            onChange={(e) => setInput({ ...input, question: e.target.value })}
-          />
-          <textarea
-            className="p-1"
-            value={input.answer}
-            onChange={(e) => setInput({ ...input, answer: e.target.value })}
-          />
-          <button className="p-1" onClick={handleSubmit}>
-            Submit
-          </button>
-          <button className="p-1" onClick={toggleEdit}>
-            Cancel
-          </button>
+        <div className="flex w-full">
+          <div className="w-full flex-1 mr-4">
+            <textarea
+              className="textarea resize-ta w-full h-28 overflow-auto resize-none border border-gray-300 p-2 text-base rounded-md"
+              value={input.question}
+              onChange={(e) => setInput({ ...input, question: e.target.value })}
+            />
+            <button
+              className="group relative w-full flex justify-center py-2 px-4 mb-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+          <div className="w-full flex-1">
+            <textarea
+              className="textarea resize-ta w-full h-28 overflow-auto resize-none border border-gray-300 p-2 text-base rounded-md"
+              value={input.answer}
+              onChange={(e) => setInput({ ...input, answer: e.target.value })}
+            />
+            <button
+              className="group relative w-full flex justify-center py-2 px-4 mb-2 border border-transparent text-sm font-medium rounded-md text-white bg-pink-400 hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={toggleEdit}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       ) : (
         <div className="flex space-x-5">
-          <div className="flex flex-col space-y-2 border-r border-black w-24">
+          <div className="flex flex-col space-y-2 w-24">
             <div className="flex space-x-2">
               <button
                 className="p-1 bg-green-500 text-white rounded hover:bg-green-700 active:bg-green-900"
@@ -71,31 +87,15 @@ const Card = ({ card, updateCard, deleteCard, changeRating }) => {
             </div>
           </div>
 
-          <div className="w-60 border-r border-black">
+          <div className="w-60">
             <div className="prose">
-              <ReactMarkdown
-                renderers={{
-                  code: ({ language, value }) => {
-                    return <SyntaxHighlighter style={solarizedlight} language={language} children={value} />;
-                  },
-                }}
-              >
-                {card.question}
-              </ReactMarkdown>
+              <ReactMarkdown>{card.question}</ReactMarkdown>
             </div>
           </div>
 
           <div className="flex-grow">
             <div className="prose">
-              <ReactMarkdown
-                renderers={{
-                  code: ({ language, value }) => {
-                    return <SyntaxHighlighter style={solarizedlight} language={language} children={value} />;
-                  },
-                }}
-              >
-                {card.answer}
-              </ReactMarkdown>
+              <ReactMarkdown>{card.answer}</ReactMarkdown>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#333',
+            a: {
+              color: '#3182ce',
+              '&:hover': {
+                color: '#2c5282',
+              },
+            },
+            pre: {
+              backgroundColor: '#282C34',
+              padding: '6px 1px 6px 12px', //top, right, bottom, left
+              borderRadius: '10px',
+            },
+            code: {
+              color: '#EB5757',
+              backgroundColor: '#e0e0e0',
+              padding: '2px 4px',
+              borderRadius: '3px',
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
